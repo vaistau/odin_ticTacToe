04cbbb00de4b284323a8b1e51f928ae4521aa0a2
Vaiva S <vaistaugaityte@gmail.com>
2022-03-16 14:23:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -18,8 +18,8 @@ const gameboard = (() => {
     })
 
     //add event listeners for each square
-    squares.forEach(square => {
-        square.addEventListener('click', () => {
+    squares.forEach(e => {
+        e.addEventListener('click', () => {
             //have to figure out how to switch between players and the appropriate symbol according to which player is active
             //square.innerHTML should be the array object of that specific index vs just editing the text alone and leaving the array empty
         })
