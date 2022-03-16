18aced1508fe84bbb9c0d0dc56200468129f3759
Vaiva S <vaistaugaityte@gmail.com>
2022-03-16 13:56:47 -0500

    update index, html

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 //gameboard object
 const gameboard = (() => {
     let board = [];
-//generate board array
+
+    //generate board array
     for (let i = 0; i < 9; i++) {
         board.push('');
     }
-})()
 
-//player factory function
-const createPlayer = (name) => {
-    return {name};
-}
+    //generate board display
+    let squares = document.querySelector('.squares');
+
+    board.forEach(element => {
+        let square = document.createElement('div');
+        square.className = 'square';
+        square.innerHTML = 'x';
+        squares.appendChild(square);
+    })
+
+    //add event listeners for each square
+
+})()
 
 //game object
 const game = (() => {
+
+    //player factory function
+    const createPlayer = (name) => {
+    return {name};
+    }
 
     //generate players
     const playerOne = createPlayer('Player 1');
